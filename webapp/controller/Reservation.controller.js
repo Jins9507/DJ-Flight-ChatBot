f7c8ede26db05a77748f1edfde6c9c77b1ef3984
Jins9507 <dydani777@gmail.com>
2024-06-05 06:28:12 +0000

    footer 버튼 아직안함

--- webapp/controller/Reservation.controller.js
+++ webapp/controller/Reservation.controller.js
@@ -1,18 +1,26 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/odata/v2/ODataModel",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox",
+	"sap/ui/core/Fragment"    
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, ODataModel, JSONModel) {
+    function (Controller, ODataModel, JSONModel, MessageBox, Fragment) {
         "use strict";
-
+        var history = {
+            prevPaymentSelect: null
+        };
+    
         return Controller.extend("dj.djchatbot.controller.Reservation", {
             onInit: function () {
                 var oTestModel = new JSONModel();
                 this._wizard = this.byId("ShoppingCartWizard");
+                this._oNavContainer = this.byId("navContainer")
+                this._oDynamicPage = this.getPage();
+
                 oTestModel = this.getOwnerComponent().getModel("testModel");
                 this.getView().setModel(oTestModel, 'testModel');
                 this.getView().setModel(new JSONModel({
@@ -20,30 +28,13 @@ sap.ui.define([
                     locationTo  :  "",
                     Passenger  : ""
                 }), "routerModel");
-                this.getView().setModel(new JSONModel([
-                        {
-                            key : "M",
-                            text  : "Male"
-                        },
-                        {
-                            key : "F",
-                            text  : "Female"
-                        }
-                ]), "genderCombo");       
-                // this.getView().setModel(new JSONModel({
-                //     "gender" : [
-                //         {
-                //             key : "M",
-                //             text  : "Male"
-                //         },
-                //         {
-                //             key : "F",
-                //             text  : "Female"
-                //         }
-                //     ]
-                // }), "genderCombo");                
+                this.getView().setModel(new JSONModel({
+                    selectedKey: "Male", // 편의상 키를 텍스트로 대체 야메야메~
+                    selectItems: [
+                        {"itemKey": "M", "itemText": "Male"},
+                        {"itemKey": "F", "itemText": "Female"}]
+                }), "genderCombo");                   
                 this.getOwnerComponent().getRouter().getRoute("Reservation").attachMatched(this._onRouteMatched, this);
-
             },
 
             _onRouteMatched: function(oEvent){
@@ -81,6 +72,10 @@ sap.ui.define([
                 });                
             },
 
+            getPage: function () {
+                return this.byId("dynamicPage");
+            },
+
             formatDate: function(oDate) {
                 var sReturnValue = "";
                 if (oDate) {
@@ -101,8 +96,7 @@ sap.ui.define([
                 var iIndex = oEvent.getParameter("selectedIndex"),
                     oView = this.getView(),
                     oReserve = this.getOwnerComponent().getModel("reserveModel").getData(),
-                    oRouterModel = oView.getModel("routerModel").getData(),
-                    oGroup = oEvent.getSource();
+                    oRouterModel = oView.getModel("routerModel").getData();
 
                 if (iIndex === 0)
                     this.getOwnerComponent().getModel("reserveModel").setProperty("/total", oReserve.reserve.price * oRouterModel.Passenger);
@@ -133,6 +127,74 @@ sap.ui.define([
                 } else {
                     this._wizard.validateStep(this.byId("CreditCardStep"));
                 }
+            },
+
+            checkPassengerStep: function () {
+                var oPassenger = this.getOwnerComponent().getModel("reserveModel").getProperty("/passenger"),
+                    vPassport  = oPassenger.passportNo || "",
+                    vName      = oPassenger.name || "",
+                    vCountry   = oPassenger.country || "",
+                    vBirth     = oPassenger.birth || "",
+                    vExpire    = oPassenger.expire || "";
+
+                if (vPassport.length < 3 || vName.length < 3 || vCountry.length < 3 || vBirth.length < 3 || vExpire.length < 3) {
+                    this._wizard.invalidateStep(this.byId("PassengerStep"));
+                } else {
+                    this._wizard.validateStep(this.byId("PassengerStep"));
+                }
+            },
+
+            setPaymentMethod: function () {
+                this.setDiscardableProperty({
+                    message: "Are you sure you want to change the payment type ? This will discard your progress.",
+                    discardStep: this.byId("PaymentTypeStep"),
+                    modelPath: "/payment",
+                    historyPath: "prevPaymentSelect"
+                });
+            },
+
+            setDiscardableProperty: function (params) {
+                if (this._wizard.getProgressStep() !== params.discardStep) {
+                    MessageBox.warning(params.message, {
+                        actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+                        onClose: function (oAction) {
+                            if (oAction === MessageBox.Action.YES) {
+                                this._wizard.discardProgress(params.discardStep);
+                                history[params.historyPath] = this.getOwnerComponent().getModel("reserveModel").getProperty(params.modelPath);
+                            } else {
+                                this.getOwnerComponent().getModel("reserveModel").setProperty(params.modelPath, history[params.historyPath]);
+                            }
+                        }.bind(this)
+                    });
+                } else {
+                    history[params.historyPath] = this.getOwnerComponent().getModel("reserveModel").getProperty(params.modelPath);
+                }
+            },
+
+            completedHandler: function () {
+                this._oNavContainer.to(this.byId("wizardBranchingReviewPage"));
+            },  
+            
+            handleNavBackToPaymentType: function () {
+                this._navBackToStep(this.byId("PaymentTypeStep"));
+            },
+    
+            handleNavBackToCreditCard: function () {
+                this._navBackToStep(this.byId("CreditCardStep"));
+            },
+    
+            handleNavBackToPassenger: function () {
+                this._navBackToStep(this.byId("PassengerStep"));
+            },   
+
+            _navBackToStep: function (step) {
+                var fnAfterNavigate = function () {
+                    this._wizard.goToStep(step);
+                    this._oNavContainer.detachAfterNavigate(fnAfterNavigate);
+                }.bind(this);
+    
+                this._oNavContainer.attachAfterNavigate(fnAfterNavigate);
+                this._oNavContainer.to(this._oDynamicPage);
             },
               
         });
